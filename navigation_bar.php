91c04a7e8e667111f93cf05ba62149176973512a
Shahriar <spranto22@gmail.com>
2021-08-17 23:22:06 +0600

    Navigation Panel

---
+++ navigation_bar.php
@@ -0,0 +1,42 @@
+	<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+	  <div class="container">
+	  	<a class="navbar-brand"  href="index.php">CVER</a>
+		  <ul class="navbar-nav">
+		    <li class="nav-item">
+		      <a class="nav-link" href="index.php">Home</a>
+		    </li>
+		    
+		    <li class="nav-item dropdown">
+		      <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+		        User
+		      </a>
+		      <div class="dropdown-menu">
+		        <a class="dropdown-item" href="user_login.php">Login</a>
+		        <a class="dropdown-item" href="user_registration.php">Register</a>
+		       
+		      </div>
+		    </li>
+
+		    <li class="nav-item dropdown">
+		      <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+		        Applications
+		      </a>
+		      <div class="dropdown-menu">
+		        <a class="dropdown-item" href="cg_application.php">Care Giver</a>
+		        <a class="dropdown-item" href="os_application.php">Oxygen Service</a>
+		       
+		      </div>
+		    </li>
+
+
+		     <li class="nav-item">
+		      <a class="nav-link" href="admin_login.php">Admin</a>
+		    </li>
+
+
+		    <li class="nav-item">
+		      <a class="nav-link" href="os_login.php">OS Service</a>
+		    </li>
+		  </ul>
+	  </div>
+	</nav>
