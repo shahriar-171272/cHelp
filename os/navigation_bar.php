129539939980c0df1c9c2056e86fa651e62f3fba
NokibulAlam <nokibul.alam02@gmail.com>
2021-08-18 04:04:24 +0600

    footer and navigation added

---
+++ os/navigation_bar.php
@@ -0,0 +1,25 @@
+	<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+	  <div class="container">
+	  	<a class="navbar-brand"  href="index.php">CVER</a>
+		  <ul class="navbar-nav">
+		    <li class="nav-item">
+		      <a class="nav-link" href="index.php">Home</a>
+		    </li>
+
+		    <li class="nav-item">
+		      <a class="nav-link" href="cylinder.php">Cylinder</a>
+		    </li>
+
+		    <li class="nav-item">
+		      <a class="nav-link" href="orders.php">Orders</a>
+		    </li>
+		    
+		     <li class="nav-item">
+		      <a class="nav-link" href="logout.php">Logout</a>
+		    </li>
+
+
+		    
+		  </ul>
+	  </div>
+	</nav>
