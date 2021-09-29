cf0910260f4732640e5bc0d65c11a6cc73110e71
Emamul Hasan <emamul@kolpolok.com>
2021-09-29 13:41:50 +0600

    Review System Design

--- review.php
+++ review.php
@@ -67,3 +67,31 @@
 			      <input type="text" class="form-control" placeholder="Enter Address" name="address">
 			    </div>
 			  </div>
+			  
+			  
+			  !DOCTYPE html>
+<html>
+<head>
+	<title>CVER | Homepage</title>
+</head>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+<body>
+
+	<?php require 'navigation_bar.php' ?>
+
+	<div class="container">
+		<div class="requirements">
+			<h3 class="text-center">Requirements for Applying in Care Giver</h3>
+			
+			<div style="margin-left: 27%;">
+				<ol>
+					<li>
+						<p style="font-weight: 700;color: red;">This candidate must complete his/her SSC examination</p>
+					</li>
+
+					<li>
+						<p style="font-weight: 700;color: red;">Must be of age 18+</p>
+						
+					</li>
