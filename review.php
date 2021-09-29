98b3489d3927e4d68f53e02d2c9a0cfe0c2bbdb4
Emamul Hasan <emamul@kolpolok.com>
2021-09-29 13:37:55 +0600

    Intregrate review in Home page

---
+++ review.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CVER | Homepage</title>
+</head>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+<body>
+
+	<?php require 'navigation_bar.php' ?>
+
+	<div class="container">
+		<div class="requirements">
+			<h3 class="text-center">Requirements for Applying in Care Giver</h3>
+			
+			<div style="margin-left: 27%;">
+				<ol>
+					<li>
+						<p style="font-weight: 700;color: red;">This candidate must complete his/her SSC examination</p>
+					</li>
+
+					<li>
+						<p style="font-weight: 700;color: red;">Must be of age 18+</p>
+						
+					</li>
+
+					
+				</ol>
+			</div>
+		</div>
+
+		<div class="registration_form">
+			<h3 class="text-center">Registration Form</h3>
+			<form action="registration_backend.php" method="POST" enctype="multipart/form-data">
+				
+			  <div class="row">
+			    <div class="col">
+			      <label>Name:</label>
+			      <input type="text" class="form-control" placeholder="Enter name" name="name">
+			    </div>
+
+			    <div class="col">
+			      <label>Email:</label>
+			      <input type="email" class="form-control" placeholder="Enter email" name="email">
+			    </div>
+
+			    <div class="col">
+			      <label>Date of Birth:</label>
+			      <input type="date" class="form-control" placeholder="Enter Birth date" name="dob">
+			    </div>
+			  </div>
+
+			  <div class="row mt-4">
+			    <div class="col">
+			      <label>Phone Number:</label>
+			      <input type="text" class="form-control" placeholder="Enter phone number" name="phone">
+			    </div>
+
+			    <div class="col">
+			      <label>NID:</label>
+			      <input type="text" class="form-control" placeholder="Enter NID no." name="nid">
+			    </div>
+
+			    <div class="col">
+			      <label>Address:</label>
+			      <input type="text" class="form-control" placeholder="Enter Address" name="address">
+			    </div>
+			  </div>
